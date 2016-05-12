6a1903386c08ad3c10ff38d2212660c03e437405
acrtson <acrtson@gmail.com>
2016-05-12 19:08:11 -0400

    Rename Input class to InputField
    added abstract class NewPage that contains functionality to fill inputs, selects and checkboxes

---
+++ src/com/jalasoft/selenium/practice02/PageObject/NewPage.java
@@ -0,0 +1,7 @@
+package com.jalasoft.selenium.practice02.PageObject;
+
+/**
+ * Created by Alex Alvarez on 5/12/2016.
+ */
+public class NewPage {
+}
--- src/com/jalasoft/selenium/practice02/Utils/InputField.java
+++ src/com/jalasoft/selenium/practice02/Utils/InputField.java
@@ -4,9 +4,9 @@ package com.jalasoft.selenium.practice02.Utils;
 /**
  * Created by Alex Alvarez on 11-05-16.
  */
-public class Input extends Field {
+public class InputField extends Field {
 
-    public Input(String identifier, String value) {
+    public InputField(String identifier, String value) {
         super(identifier, value);
     }
 
